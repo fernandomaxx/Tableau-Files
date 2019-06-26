e33ec7ab5e2928d4732e0292af50e58b0fef6e2d
fernandomaxx <32984809+fernandomaxx@users.noreply.github.com>
2019-06-26 14:50:22 -0300

    update disease_data_generator.py 1.01

--- scripts/disease_data_generator.py
+++ scripts/disease_data_generator.py
@@ -1,32 +1,38 @@
 filename = input()
 mfilename = input()
+dfilename = input()
 out_filename = input()
 f_size = int(input())
 features = [[] for _ in range(f_size)]
 f_file = open(filename)
 m_file = open(mfilename)
+d_file = open(dfilename)
 out_file = open(out_filename, "w")
 features_name = []
 f_pointer = []
 disease_name = []
-m_names = m_file.readlines()
-m_size = len(m_names)
+m_names = []
 count = 0
 
-print(m_size)
-print(m_names)
+for line in m_file:
+    m_names.append(line.replace("\n", ""))
 
+m_size = len(m_names)
+
+disease_name = d_file.readlines()
+
+'''
 while True:
     name = input()
     if name == "exit":
        break
     else:
         disease_name.append(name)
-print("passou")
+'''
+
 for i in range(0, f_size):
     features_name.append(input())
     f_pointer.append(0)
-print("passou")
 
 for line in f_file.readlines():
     line = line.replace("\n", "")
@@ -34,17 +40,21 @@ for line in f_file.readlines():
     for i in range(0, f_size):
         features[i].append(values[i])
 
-print(len(features[0]))
-print("passou")
-
-
-for i in range(0, len(features[0])/m_size):
+d_count = 0
+for i in range(0, int(len(features[0])/m_size) * f_size):
     f_count = i%f_size
     for j in range(0, m_size):
-        line = count + ";" + m_names[j] + ";" + features_name[f_count] + ";" + features[f_count][f_pointer[f_count]] + ";" + disease_name[f_count] + "\n"
+        line = str(count) + ";" + m_names[j] + ";" + features_name[f_count] + ";" + features[f_count][f_pointer[f_count]] + ";" + disease_name[d_count]
+        print(line)
         out_file.write(line)
         f_pointer[f_count] += 1
         count += 1
+    if f_count == f_size - 1:
+        d_count += 1
+        
+f_file.close()
+m_file.close()
+out_file.close()
                
 
 
